9faca11e047110ffa3e9d3555c213d160fe7f6a1
hcpthanks <hcpthanks@163.com>
2017-08-12 20:53:43 +0800

    update django_moudle_sample

--- Django_moudle_sample/tutorial/feedback/views.py
+++ Django_moudle_sample/tutorial/feedback/views.py
@@ -9,4 +9,26 @@ def home(request):
     context = {
         'categories': Feedback.CATEGORIES,
     }
+    if request.method == 'POST':
+        f = Feedback()
+        f.subject = request.POST.get('subject')
+        f.category = request.POST.get('category')
+        f.username = request.POST.get('username')
+        f.email = request.POST.get('email')
+        f.description = request.POST.get('description')
+        f.screenshot = ''
+        f.subscription = 'subscription' in request.POST
+        f.status = '待处理'
+        f.save()
+        return HttpResponse('问题提交成功')
+
     return render(request, 'feedback/feedback.html', context)
+
+
+def get_feedback_data(request):
+    if request.method == 'GET':
+        data = dict(request.GET)
+        return HttpResponse(str(data))
+
+
+
