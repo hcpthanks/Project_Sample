fdd9724872cb2305fbd9a5cab521f92d0c07306d
hcpthanks <hcpthanks@163.com>
2017-09-04 09:43:12 +0800

    update vcard

--- Vcard(django)/vcard/blog/views.py
+++ Vcard(django)/vcard/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Category, Post, Comment
 
 # Create your views here.
@@ -7,8 +8,18 @@ from .models import Category, Post, Comment
 def blog_list(request):
     """博客列表页
     """
-    posts = Post.published_objects.all()
-    return render(request, 'blog-list.html', {'posts': posts})
+    # posts = Post.published_objects.all()
+    # return render(request, 'blog-list.html', {'posts': posts})
+    object_list = Post.published_objects.all()
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog-list.html',{'page':page,'posts':posts,'paginator':paginator})
 
 def blog_detail(request,year, month, day, slug):
     """博客日志详情页
